ad15b2506a6cf92b6b2864417e90a89ea3eec637
Jonathan Daugherty <jtd@galois.com>
2017-04-14 15:53:14 -0700

    dismantle-tablegen: make parser tests discover and parse tgen files in sibiling projects

--- dismantle-tablegen/tests/Parser.hs
+++ dismantle-tablegen/tests/Parser.hs
@@ -1,37 +1,61 @@
+{-# LANGUAGE TupleSections #-}
 module Parser ( parserTests ) where
 
 import Control.DeepSeq ( deepseq )
+import Control.Monad (when, forM)
+import Data.Monoid ((<>))
 import qualified Data.Text.Lazy.IO as TL
 import qualified Test.Tasty as T
 import qualified Test.Tasty.HUnit as T
+import qualified System.FilePath.Glob as G
+import System.Directory (canonicalizePath)
+import System.Exit (die)
+import System.FilePath (takeFileName)
 
 import qualified Dismantle.Tablegen as D
 
 parserTests :: IO T.TestTree
 parserTests = do
-  return (T.testGroup "Parser" (map mkTest isaTable))
+  tgenFiles <- findTgenFiles
+  (T.testGroup "Parser") <$> mapM mkParserTestGroup tgenFiles
 
-mkTest :: (FilePath, D.ISA) -> T.TestTree
-mkTest (p, isa) = T.testCase p $ do
+mkParserTestGroup :: (String, [FilePath]) -> IO T.TestTree
+mkParserTestGroup (pkg, files) = do
+  return (T.testGroup pkg (map mkTest files))
+
+requireGlob :: String -> String -> IO [FilePath]
+requireGlob ty pat = do
+    paths <- mapM canonicalizePath =<< G.glob pat
+    when (null paths) $ do
+        die $ "Error: could not find any i" <> ty <> " matching " <> show pat
+    return paths
+
+-- Find all .tgen files in this package and sibling packages, assuming
+-- these tests are being run from a dismantle-tablegen build directory
+-- where sibling dismantle-* projects are in the same directory as
+-- dismantle-tablegen.
+--
+-- Returns the tablegen files it found, paired with the path to the
+-- project that provides them.
+--
+-- Dies if no tablegen source files could be found.
+findTgenFiles :: IO [(FilePath, [FilePath])]
+findTgenFiles = do
+    projectPaths <- requireGlob "dismantle projects" "../dismantle-*"
+
+    forM projectPaths $ \project -> do
+        let tgenPattern = project <> "/data/*.tgen"
+        (project,) <$> requireGlob "tgen files" tgenPattern
+
+mkTest :: FilePath -> T.TestTree
+mkTest p = T.testCase (takeFileName p) $ do
   t <- TL.readFile p
   case D.parseTablegen p t of
-    Left err -> T.assertFailure (show err)
-    Right rs -> do
-      let insns = D.filterISA isa rs
-      insns `deepseq` return ()
-      case D.makeParseTables isa insns of
-        Left err -> T.assertFailure (show err)
-        Right tbl -> tbl `seq` return ()
-
-isaTable :: [(FilePath, D.ISA)]
-isaTable = [ ("data/AArch64.tgen", D.aarch64)
-           , ("data/ARM.tgen", D.arm)
-           , ("data/ARM.tgen", D.thumb)
-           , ("data/AVR.tgen", D.avr)
-           , ("data/Mips.tgen", D.mips)
-           , ("data/PPC.tgen", D.ppc)
-           , ("data/Sparc.tgen", D.sparc)
-           ]
+    Left err ->
+        let msg = "Error parsing " <> show p <> ": " <> show err
+        in T.assertFailure msg
+    Right rs ->
+        rs `deepseq` return ()
 
 {-
 
