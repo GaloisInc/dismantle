814f5f24919c537f7a9b75b6090307f249773021
Jonathan Daugherty <jtd@galois.com>
2017-04-17 09:31:21 -0700

    dismantle-arm: add a script to extract and classify ARM instruction operand types

---
+++ dismantle-arm/scripts/operand_types.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+HERE=$(cd `dirname $0`; pwd)
+
+TGEN=$HERE/../data/ARM.tgen
+
+grep OperandList $TGEN \
+    | awk -F'= ' '{ print $2 }' \
+    | sed 's/ins //g' \
+    | sed 's/outs //g' \
+    | sed 's/(//' \
+    | sed 's/);//' \
+    | grep -v ins \
+    | grep -v outs \
+    | awk -F', ' '{ for (i = 1; i <= NF; i++) { print $i } }' \
+    | grep -v variable_ops \
+    | awk -F':' '(NF == 2) { print "concrete:" $1 } (NF > 2) { print "indirect:" $0 }'
