5deac2189bb07e01eca00348e80a2a65cff00f14
Jonathan Daugherty <jtd@galois.com>
2017-04-17 10:09:12 -0700

    dismantle-arm: improve selectivity of operand type script, document each pipeline stage

--- dismantle-arm/scripts/operand_types.sh
+++ dismantle-arm/scripts/operand_types.sh
@@ -14,16 +14,57 @@ HERE=$(cd `dirname $0`; pwd)
 
 TGEN=$HERE/../data/ARM.tgen
 
-grep OperandList $TGEN \
-    | awk -F'= ' '{ print $2 }' \
-    | sed 's/ins //g' \
-    | sed 's/outs //g' \
-    | sed 's/(//' \
-    | sed 's/);//' \
-    | grep -v ins \
-    | grep -v outs \
-    | awk -F', ' '{ for (i = 1; i <= NF; i++) { print $i } }' \
-    | grep -v variable_ops \
-    | awk -F':' '(NF == 2) { print "concrete:" $1 } (NF == 3) { print "indirect:" $1 ":" $2 }' \
-    | sort \
-    | uniq
+# Expected line structure:
+# dag OutOperandList = (outs QPR:$Vd);
+# dag InOperandList = (ins QPR:$Vm);
+
+# Only process lines that mention operand lists. This rules out lines like
+# dag InOperandList = !con(AExtI:iops, (ins pred:$p));
+grep OperandList $TGEN | {
+    # Extract the right hand side of each operand list assignment.
+    awk -F'= ' '{ print $2 }'
+} | {
+    # Permit only lines that begin with ins/outs (remove lines like
+    # "!con(...")
+    grep -E '^\((ins|outs)'
+} | {
+    # Remove ins keywords
+    sed 's/ins //g'
+} | {
+    # Remove outs keywords
+    sed 's/outs //g'
+} | {
+    # Remove parens
+    sed 's/^(//'
+} | {
+    sed 's/);$//'
+} | {
+    # At this point, lines that initially read "... = (ins);" will just
+    # say "ins", so we need to remove those.
+    grep -vx ins
+} | {
+    # At this point, lines that initially read "... = (outs);" will just
+    # say "outs", so we need to remove those.
+    grep -vx outs
+} | {
+    # For each argument in "a:b, c:d, ...", print one argument per line.
+    awk -F', ' '{ for (i = 1; i <= NF; i++) { print $i } }'
+} | {
+    # Remove variable_ops arguments.
+    grep -v variable_ops
+} | {
+    # For each line of the form "a:b", prepend a concrete prefix since
+    # we think those refer to concrete operand types. For each line of
+    # the form "a:b:c", prepend an indirect prefix since those typically
+    # refer to types of class attributes. If we encounter one with more
+    # components than expected, use the prefix "bad:".
+    awk -F':' '(NF == 2) { print "concrete:" $1 } \
+               (NF == 3) { print "indirect:" $1 ":" $2 } \
+               (NF > 3)  { print "bad:" $0 }'
+} | {
+    # Sort ...
+    sort
+} | {
+    # ... and eliminate duplicates.
+    uniq
+}
