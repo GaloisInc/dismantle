6c98ba7007e132180ea171319a2068d2d621fe05
Tristan Ravitch <tristan@nochair.net>
2017-05-10 15:37:43 -0700

    Do a better job of suppressing test output for travis
    Travis was collecting all of the output from the tests and killing the job once
    it hit 4MB.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,16 @@ case $ARCH in
         stack build dismantle-tablegen --system-ghc --test
         ;;
     ppc)
-        stack build dismantle-ppc --system-ghc --test --test-arguments='--hide-successes'
+        # Travis pretends to offer a fully functional TTY, but it slurps up all
+        # of the output for logging.  With --hide-successes, tasty uses TTY
+        # magic to rewrite the same line repeatedly to prevent scrolling.  These
+        # two things seem to interact poorly, and travis logs every line that
+        # eventually gets rewritten; once it hits 4MB, it kills the job.
+        #
+        # Pipe the output of the test suite to cat so that tasty doesn't think
+        # it has a TTY and really doesn't produce any success output.  This has
+        # the nice side effect of being much faster.
+        stack build dismantle-ppc --system-ghc --test --test-arguments='--hide-successes' | cat -
         ;;
     *)
         echo "Unrecognized TEST_ARCH=${ARCH}"
