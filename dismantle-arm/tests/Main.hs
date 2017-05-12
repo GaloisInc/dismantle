6ee896f3a47800b26f0d7f752ae271968191ff90
Jonathan Daugherty <jtd@galois.com>
2017-05-12 10:27:02 -0700

    dismantle-arm: make MVN expected fail comment more precise

--- dismantle-arm/tests/Main.hs
+++ dismantle-arm/tests/Main.hs
@@ -31,9 +31,11 @@ rx s =
 expectedFailures :: RE.RE
 expectedFailures = rx (L.intercalate "|" rxes)
   where
-    rxes = [ -- The tablegen data for MVNpl is currently incorrect
-             -- w.r.t. unpredictable bits. A bug has been submitted
-             -- upstream. In the mean time, for details, see
+    rxes = [ -- The tablegen data for MVN is currently incorrect
+             -- w.r.t. unpredictable bits. The only variant of
+             -- MVN that we've encountered that actually sets
+             -- some unpredictable bits is 'mvnpl'. A bug has been
+             -- submitted upstream. In the mean time, for details, see
              -- https://bugs.llvm.org/show_bug.cgi?id=33011
              "^[[:space:]]*mvnpl"
            ]
