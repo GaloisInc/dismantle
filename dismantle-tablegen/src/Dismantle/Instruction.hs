8023d786fbcd46668de5fff938a04f32f279c624
Tristan Ravitch <tristan@nochair.net>
2017-10-23 17:08:13 -0700

    Remove some dead code

--- dismantle-tablegen/src/Dismantle/Instruction.hs
+++ dismantle-tablegen/src/Dismantle/Instruction.hs
@@ -9,22 +9,10 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Dismantle.Instruction (
-  -- OperandList(..),
   GenericInstruction(..),
   Annotated(..),
   OpcodeConstraints,
-  -- Index(..),
-  -- mapOpcode,
-  traverseOpcode,
-  -- operandListLength,
-  -- mapOperandList,
-  -- mapOperandListIndexed,
-  -- traverseOperandList,
-  -- traverseOperandListIndexed,
-  -- foldrOperandList,
-  -- foldrMOperandList,
-  -- indexOpList,
-  -- indexAsInt
+  traverseOpcode
   ) where
 
 import qualified Data.Type.Equality as E
@@ -78,32 +66,6 @@ instance (E.TestEquality o, OrdF (t o), OrdF (ShapedList o)) => Ord (GenericInst
   Instruction op1 args1 `compare` Instruction op2 args2 =
     (Some op1, Some args1) `compare` (Some op2, Some args2)
 
--- -- | An implementation of heterogeneous lists for operands, with the
--- -- types of operands (caller-specified) reflected in the list type.
--- -- data OperandList f sh where
--- data OperandList :: (k -> *) -> [k] -> * where
---   Nil  :: OperandList f '[]
---   (:>) :: f tp -> OperandList f tps -> OperandList f (tp ': tps)
-
--- infixr 5 :>
-
--- instance (ShowF o) => Show (OperandList o sh) where
---   show Nil = "Nil"
---   show (elt :> rest) = showF elt ++ " :> " ++ show rest
-
--- instance (ShowF o) => ShowF (OperandList o)
-
--- instance (E.TestEquality o) => E.TestEquality (OperandList o) where
---   testEquality Nil Nil = Just E.Refl
---   testEquality (i1 :> rest1) (i2 :> rest2) =
---     case E.testEquality i1 i2 of
---       Just E.Refl ->
---         case E.testEquality rest1 rest2 of
---           Just E.Refl -> Just E.Refl
---           Nothing -> Nothing
---       Nothing -> Nothing
---   testEquality _ _ = Nothing
-
 instance (E.TestEquality (c o), E.TestEquality o) => Eq (GenericInstruction c o) where
   Instruction o1 ops1 == Instruction o2 ops2 =
     case E.testEquality o1 o2 of
@@ -122,94 +84,6 @@ instance (ShowF (c o), ShowF o) => Show (GenericInstruction c o) where
            , ")"
            ]
 
--- -- | A type parameterized map
--- mapOperandList :: (forall tp . a tp -> b tp) -> OperandList a sh -> OperandList b sh
--- mapOperandList f l =
---   case l of
---     Nil -> Nil
---     e :> rest -> f e :> mapOperandList f rest
-
--- mapOperandListIndexed :: forall a b sh
---                        . (forall tp . Index sh tp -> a tp -> b tp)
---                       -> OperandList a sh
---                       -> OperandList b sh
--- mapOperandListIndexed f = go id
---   where
---     go :: forall tps . (forall tp . Index tps tp -> Index sh tp)
---        -> OperandList a tps
---        -> OperandList b tps
---     go g l =
---       case l of
---         Nil -> Nil
---         e :> rest -> f (g IndexHere) e :> go (\ix -> g (IndexThere ix)) rest
-
--- traverseOperandList :: (Applicative t)
---                     => (forall tp . a tp -> t (b tp))
---                     -> OperandList a sh
---                     -> t (OperandList b sh)
--- traverseOperandList f l =
---   case l of
---     Nil -> pure Nil
---     e :> rest -> (:>) <$> f e <*> traverseOperandList f rest
-
--- traverseOperandListIndexed :: forall a b sh t
---                             . (Applicative t)
---                            => (forall tp . Index sh tp -> a tp -> t (b tp))
---                            -> OperandList a sh
---                            -> t (OperandList b sh)
--- traverseOperandListIndexed f = go id
---   where
---     go :: forall tps . (forall tp . Index tps tp -> Index sh tp)
---        -> OperandList a tps
---        -> t (OperandList b tps)
---     go g l =
---       case l of
---         Nil -> pure Nil
---         e :> rest -> (:>) <$> f (g IndexHere) e <*> go (\ix -> g (IndexThere ix)) rest
-
--- foldrOperandList :: forall sh a b . (forall tp . Index sh tp -> a tp -> b -> b) -> b -> OperandList a sh -> b
--- foldrOperandList f seed0 l = go id l seed0
---   where
---     go :: forall tps
---         . (forall tp . Index tps tp -> Index sh tp)
---        -> OperandList a tps
---        -> b
---        -> b
---     go g ops b =
---       case ops of
---         Nil -> b
---         a :> rest -> f (g IndexHere) a (go (\ix -> g (IndexThere ix)) rest b)
-
--- foldrMOperandList :: forall sh a b m . (Monad m)
---                   => (forall tp . Index sh tp -> a tp -> b -> m b)
---                   -> b
---                   -> OperandList a sh
---                   -> m b
--- foldrMOperandList f seed0 l = go id l seed0
---   where
---     go :: forall tps
---         . (forall tp . Index tps tp -> Index sh tp)
---        -> OperandList a tps
---        -> b
---        -> m b
---     go g ops b =
---       case ops of
---         Nil -> return b
---         a :> rest -> f (g IndexHere) a =<< go (\ix -> g (IndexThere ix)) rest b
-
--- -- | Return the number of operands in an operand list.
--- --
--- -- O(n)
--- operandListLength :: OperandList a sh -> Int
--- operandListLength Nil = 0
--- operandListLength (_ :> rest) = 1 + operandListLength rest
-
--- -- | Map over opcodes in a shape-preserving way
--- mapOpcode :: (forall (sh :: [k]) . c o sh -> c o sh) -> GenericInstruction c o -> GenericInstruction c o
--- mapOpcode f i =
---   case i of
---     Instruction op ops -> Instruction (f op) ops
-
 -- | Map over opcodes while preserving the shape of the operand list, allowing effects
 traverseOpcode :: (Applicative t)
                => (forall (sh :: [k]) . c o sh -> t (c o sh))
@@ -224,46 +98,3 @@ type OpcodeConstraints c o = (E.TestEquality (c o),
                               EnumF (c o),
                               Typeable c,
                               Typeable o)
-
--- -- | Represents an index into a type-level list. Used in place of integers to
--- --   1. ensure that the given index *does* exist in the list
--- --   2. guarantee that it has the given kind
--- data Index :: [k] -> k -> * where
---   IndexHere :: forall x sh. Index (x ': sh) x
---   IndexThere :: forall x x' sh. Index sh x -> Index (x' ': sh) x
--- deriving instance Eq (Index sh x)
--- deriving instance Show (Index sh x)
-
--- instance ShowF (Index sh)
-
--- instance E.TestEquality (Index sh) where
---   IndexHere `testEquality` IndexHere = Just E.Refl
---   IndexThere idx1 `testEquality` IndexThere idx2 = E.testEquality idx1 idx2
---   _ `testEquality` _ = Nothing
-
--- instance OrdF (Index sh) where
---   IndexHere `compareF` IndexHere = EQF
---   IndexHere `compareF` IndexThere _ = LTF
---   IndexThere _ `compareF` IndexHere = GTF
---   IndexThere idx1 `compareF` IndexThere idx2 =
---     case idx1 `compareF` idx2 of
---       LTF -> LTF
---       EQF -> EQF
---       GTF -> GTF
-
--- instance Ord (Index sh x) where
---   x `compare` y = toOrdering $ x `compareF` y
-
--- -- | Evaluate an index for a given operand list.
--- indexOpList :: OperandList f sh -> Index sh s -> f s
--- -- Why not destructure @vals@ in the argument position? GHC gives a warning
--- -- about not handling the Nil case of vals. This way, GHC verifies that the
--- -- pattern-matching is exhaustive.
--- indexOpList vals IndexHere = case vals of x :> _ -> x
--- indexOpList vals (IndexThere th) = case vals of _ :> rest -> indexOpList rest th
-
--- indexAsInt :: Index sh tp -> Int
--- indexAsInt ix =
---   case ix of
---     IndexHere -> 0
---     IndexThere ix' -> 1 + indexAsInt ix'
