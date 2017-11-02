51fc028758a2484ce4b659d9bec912e540f7b30b
Jonathan Daugherty <jtd@galois.com>
2017-11-02 09:22:54 -0700

    thumb: get test suite building

--- dismantle-thumb/tests/Main.hs
+++ dismantle-thumb/tests/Main.hs
@@ -12,120 +12,27 @@ import Data.Word (Word64)
 
 import Dismantle.Testing
 
-import qualified Dismantle.ARM as ARM
+import qualified Dismantle.Thumb as Thumb
 
 ignored :: [(FilePath, [Word64])]
 ignored =
-    [ ("tests/bin/ls"
-      , [ 0x1e198
-        -- This address is ignored because objdump considers this byte
-        -- sequence to be a 'tst' instruction, but that is incorrect.
-        -- The ARM ARM specifies this as an MRS instruction. We parse it
-        -- correctly.
-        -- 1e198:       01010101        tsteq   r1, r1, lsl #2
-        ]
-      )
-    , ("tests/bin/libfftw3.so.3.4.4"
-      , [
-        -- These addresses are ignored because objdump considers them to
-        -- be 'cmn' instructions but that is clearly wrong in the ARM
-        -- ARM.
-          0x9a948
-        , 0xea750
-        , 0x131780
-        , 0x181300
-
-        -- These addresses all use the same non-instruction byte
-        -- sequence: 0x3c182b51. objdump claims this disassembles to
-        -- ldccc, but none of the ARM ARM patterns for LDC match this
-        -- sequence.
-        , 0x36408
-        , 0x372e8
-        , 0x37718
-        , 0x37b48
-        , 0x5a498
-        , 0x5a4e8
-        , 0x5b220
-        , 0x5b698
-        , 0x7b520
-        , 0x7b570
-        , 0x7c298
-        , 0x7c728
-        , 0xb07f0
-        , 0xb0c38
-        , 0xb0cb8
-        , 0xb1978
-        , 0xb19c0
-        , 0xd41d0
-        , 0xd4690
-        , 0xd5820
-        , 0xe6cb8
-        , 0xe7180
-        , 0xe82f8
-        , 0xf5ee0
-        , 0xf6358
-        , 0xf63a0
-        , 0xf6f78
-        , 0xf6fa8
-        , 0x1488d0
-        , 0x148d20
-        , 0x168d40
-        , 0x169df8
-        , 0x17ba78
-        , 0x17ca88
-        , 0x18cab8
-        , 0x18cec8
-
-        -- These addresses are considered 'sbc' instructions by objdump,
-        -- but we disassemble them as 'adrpc' legally.
-        , 0x1b7d8
-        , 0x3c9f0
-        , 0x9a928
-        , 0xb7098
-        , 0xea738
-        , 0x1319e8
-        , 0x14d0f0
-        , 0x181308
-
-        -- These addresses are considered to be 'and' instructions by
-        -- objdump, but we disassemble them as 'adr'. objdump is wrong.
-        , 0x20270
-        , 0x20720
-        , 0x20748
-        , 0x20fa0
-        , 0x9d298
-        , 0x9d2a0
-        , 0x133ea8
-        , 0x133ee8
-
-        -- Objdump claims "strdvc" but the Tgen says otherwise for that
-        -- instruction.
-        , 0x9a930
-        , 0xea740
-        , 0x131778
-        , 0x1812f8
-
-        -- This location is handled properly by our library, but our
-        -- pretty-print for it is nicer than objdump.
-        , 0xb0ec
-        ]
-      )
+    [
     ]
 
-arm :: ArchTestConfig
-arm = ATC { archName = "arm"
-          , disassemble = ARM.disassembleInstruction
-          , assemble = ARM.assembleInstruction
-          , prettyPrint = ARM.ppInstruction
-          , expectFailure = Just expectedFailures
-          , skipPrettyCheck = Just skipPretty
-          , ignoreAddresses = ignored
-          , normalizePretty = normalize
-          }
+thumb :: ArchTestConfig
+thumb = ATC { archName = "thumb"
+            , disassemble = Thumb.disassembleInstruction
+            , assemble = Thumb.assembleInstruction
+            , prettyPrint = Thumb.ppInstruction
+            , expectFailure = Just expectedFailures
+            , skipPrettyCheck = Just skipPretty
+            , ignoreAddresses = ignored
+            , normalizePretty = normalize
+            }
 
 main :: IO ()
 main = do
-  tg <- binaryTestSuite arm "tests/bin"
+  tg <- binaryTestSuite thumb "tests/bin"
   T.defaultMain tg
 
 normalize :: TL.Text -> TL.Text
@@ -242,7 +149,7 @@ skipPretty = rx (L.intercalate "|" rxes)
     matchInstruction name = "(^[[:space:]]*" <> name <> conditions <> "[[:space:]])"
 
     conditions = "(" <> (concat $ L.intersperse "|"
-                  (PP.render <$> PP.pPrint <$> ARM.mkPred <$> [0..13])) <> ")?"
+                  (PP.render <$> PP.pPrint <$> Thumb.mkPred <$> [0..13])) <> ")?"
 
 expectedFailures :: RE.RE
 expectedFailures = rx (L.intercalate "|" rxes)
