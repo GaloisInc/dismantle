bcc18b4a51e2d176d5eb1408bd714b921a298ed7
Jonathan Daugherty <jtd@galois.com>
2017-11-27 12:12:12 -0800

    aarch64: add SUB overrides and operand types

--- dismantle-aarch64/src/Dismantle/AArch64/ISA.hs
+++ dismantle-aarch64/src/Dismantle/AArch64/ISA.hs
@@ -133,6 +133,8 @@ isa = ISA { isaName = "AArch64"
         , ("Fpimm64", AArch64.fpimm64Operand)
         , ("Logical_shifted_reg32", AArch64.gPR32Operand)
         , ("Logical_shifted_reg64", AArch64.gPR64Operand)
+        , ("Arith_shifted_reg32", AArch64.gPR32Operand)
+        , ("Arith_shifted_reg64", AArch64.gPR64Operand)
         ]
 
     aarch64Filter = hasNamedString "Namespace" "AArch64" &&&
