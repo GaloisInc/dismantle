db5366fe45d75a22f1264df5cdcea2450c40ed5c
Tristan Ravitch <tristan@ravit.ch>
2019-08-14 10:26:23 -0700

    Introduce a monadic context for name generation
    There are a few opcode names that are re-used in the semantics (for opcodes with
    different bit patterns).  We need to disambiguate the names.

--- dismantle-asl/src/Dismantle/ASL.hs
+++ dismantle-asl/src/Dismantle/ASL.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Dismantle.ASL (
   loadASL,
@@ -10,9 +11,11 @@ module Dismantle.ASL (
   ) where
 
 import           Control.Monad ( when )
+import qualified Control.Monad.State.Strict as MS
 import           Data.Either ( either )
 import qualified Data.Foldable as F
-import           Data.Maybe ( fromMaybe, mapMaybe )
+import qualified Data.Map as M
+import           Data.Maybe ( catMaybes, fromMaybe )
 import qualified Data.Set as S
 import qualified Data.Text as T
 import           Data.Void ( Void )
@@ -20,51 +23,76 @@ import           Data.Word ( Word8 )
 import qualified Text.Megaparsec as P
 import           Text.Printf ( printf )
 import qualified Language.ASL.Syntax as AS
--- import qualified Language.ASL.Parser as AP
 
 import qualified Dismantle.Tablegen as DT
 import qualified Dismantle.Tablegen.ByteTrie as BT
 import qualified Dismantle.Tablegen.Parser.Types as PT
 
+data ASLState = ASLState { usedNames :: M.Map String Int
+                         }
+
+newtype ASL a = ASL (MS.State ASLState a)
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MS.MonadState ASLState
+           )
+
+runASL :: ASL a -> a
+runASL (ASL a) = MS.evalState a (ASLState M.empty)
+
 loadASL :: (AS.InstructionEncoding -> Bool) -> String -> [AS.Instruction] -> DT.ISADescriptor
-loadASL fltr arch insns =
-  DT.ISADescriptor { DT.isaInstructions = instrs
-                   , DT.isaOperands = S.toList (S.fromList (concatMap instrOperandTypes instrs))
-                   , DT.isaErrors = []
-                   }
-  where
-    instrs = concatMap (aslToInsnDesc fltr arch) insns
+loadASL fltr arch insns = runASL $ do
+  instrs <- concat <$> mapM (aslToInsnDesc fltr arch) insns
+  return $ DT.ISADescriptor { DT.isaInstructions = instrs
+                            , DT.isaOperands = S.toList (S.fromList (concatMap instrOperandTypes instrs))
+                            , DT.isaErrors = []
+                            }
 
 instrOperandTypes :: DT.InstructionDescriptor -> [DT.OperandType]
 instrOperandTypes idesc = map DT.opType (DT.idInputOperands idesc ++ DT.idOutputOperands idesc)
 
-aslToInsnDesc :: (AS.InstructionEncoding -> Bool) -> String -> AS.Instruction -> [DT.InstructionDescriptor]
-aslToInsnDesc fltr arch i = mapMaybe (encodingToInstDesc fltr arch) (AS.instEncodings i)
+aslToInsnDesc :: (AS.InstructionEncoding -> Bool) -> String -> AS.Instruction -> ASL [DT.InstructionDescriptor]
+aslToInsnDesc fltr arch i =
+  catMaybes <$> mapM (encodingToInstDesc fltr arch) (AS.instEncodings i)
 
 encodingToInstDesc :: (AS.InstructionEncoding -> Bool)
                    -> String
                    -> AS.InstructionEncoding
-                   -> Maybe DT.InstructionDescriptor
+                   -> ASL (Maybe DT.InstructionDescriptor)
 encodingToInstDesc fltr arch e
-  | not (fltr e) = Nothing
-  | otherwise = Just $
-  DT.InstructionDescriptor { DT.idMask = fixedEncodingMask e
-                           , DT.idNegMask = negativeEncodingMask e
-                           , DT.idMnemonic = encodingMnemonic e
-                           , DT.idInputOperands = map toOperandDescriptor (AS.encFields e)
-                           , DT.idOutputOperands = [] -- See Note [Output Operands]
-                           , DT.idNamespace = arch
-                           , DT.idDecoderNamespace = show (AS.encInstrSet e)
-                           , DT.idAsmString = asmString e
-                           , DT.idPseudo = False
-                           , DT.idDefaultPrettyVariableValues = []
-                           , DT.idPrettyVariableOverrides = []
-                           }
+  | not (fltr e) = return Nothing
+  | otherwise = do
+      mnemonic <- encodingMnemonic e
+      let desc = DT.InstructionDescriptor
+                 { DT.idMask = fixedEncodingMask e
+                 , DT.idNegMask = negativeEncodingMask e
+                 , DT.idMnemonic = mnemonic
+                 , DT.idInputOperands = map toOperandDescriptor (AS.encFields e)
+                 , DT.idOutputOperands = [] -- See Note [Output Operands]
+                 , DT.idNamespace = arch
+                 , DT.idDecoderNamespace = show (AS.encInstrSet e)
+                 , DT.idAsmString = asmString e mnemonic
+                 , DT.idPseudo = False
+                 , DT.idDefaultPrettyVariableValues = []
+                 , DT.idPrettyVariableOverrides = []
+                 }
+
+      return (Just desc)
 
 type Parser = P.Parsec Void T.Text
 
-encodingMnemonic :: AS.InstructionEncoding -> String
-encodingMnemonic e = T.unpack (either (const (AS.encName e)) id (P.runParser nameParser "" (AS.encName e)))
+encodingMnemonic :: AS.InstructionEncoding -> ASL String
+encodingMnemonic e = do
+  let mn0 = T.unpack (either (const (AS.encName e)) id (P.runParser nameParser "" (AS.encName e)))
+  names <- MS.gets usedNames
+  case M.lookup mn0 names of
+    Nothing -> do
+      MS.modify' $ \s -> s { usedNames = M.insert mn0 0 (usedNames s) }
+      return mn0
+    Just nUses -> do
+      MS.modify' $ \s -> s { usedNames = M.insertWith (\_ oldCount -> oldCount + 1) mn0 0 (usedNames s) }
+      return (printf "%s_%d" mn0 nUses)
 
 nameParser :: Parser T.Text
 nameParser = do
@@ -80,8 +108,8 @@ nameParser = do
 -- (i.e., dropping the aarch_ prefix and any suffixes)
 --
 -- FIXME: Add in field format slots
-asmString :: AS.InstructionEncoding -> String
-asmString e = encodingMnemonic e
+asmString :: AS.InstructionEncoding -> String -> String
+asmString e mn = mn
 
 -- | Negative masks come from guards on fields.
 negativeEncodingMask :: AS.InstructionEncoding -> [BT.Bit]
