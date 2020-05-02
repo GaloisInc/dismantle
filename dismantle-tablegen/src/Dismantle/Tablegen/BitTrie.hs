de646b4ebeeb3444ef0d4c69b62cf751f5b49040
Tristan Ravitch <tristan@ravit.ch>
2020-05-01 17:47:16 -0700

    Fix some more logic around negative pattern matching
    This commit is more faithful to the semantics of negative pattern matching.
    Negative patterns can only reject a matching bitstring if the bitstring is long
    enough to include *all* of the negative bits in the pattern.
    
    It also fixes some of the matching logic to not only give early matching
    preference to short patterns that fully cover the bit string, but also filters
    out shorter paths once the bit string grows larger than the pattern.
    
    This commit rewrites the `makePayload` function to attempt to make its logic
    easier to follow.

--- dismantle-tablegen/src/Dismantle/Tablegen/BitTrie.hs
+++ dismantle-tablegen/src/Dismantle/Tablegen/BitTrie.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MultiWayIf #-}
 module Dismantle.Tablegen.BitTrie (
   bitTrie,
   lookupByte
@@ -6,7 +7,7 @@ module Dismantle.Tablegen.BitTrie (
 import qualified Control.Exception as X
 import qualified Control.Monad.Except as ME
 import qualified Control.Monad.State.Strict as St
-import           Data.Bits ( (.|.), (.&.), popCount, testBit )
+import           Data.Bits ( (.|.), (.&.), popCount, testBit, countLeadingZeros )
 import qualified Data.Bits as DB
 import qualified Data.ByteString as BS
 import qualified Data.Foldable as F
@@ -92,13 +93,13 @@ buildTableLevel patterns bitIndex bitsSoFar = do
           Just s -> s .|. ps
   let pset = F.foldl' addPatternToSet (DTP.PatternSet 0) (M.keys patterns)
   let key = (bitIndex, pset)
-  case HM.lookup key cache of
-    Just tix -> return tix
-    Nothing -> do
-      payloads <- T.traverse (makePayload patterns bitIndex bitsSoFar) [False, True]
-      tix <- newTable payloads
-      St.modify' $ \s -> s { DTP.tsCache = HM.insert key tix (DTP.tsCache s) }
-      return tix
+  -- case HM.lookup key cache of
+  --   Just tix -> return tix
+  --   Nothing -> do
+  payloads <- T.traverse (makePayload patterns bitIndex bitsSoFar) [False, True]
+  tix <- newTable payloads
+  St.modify' $ \s -> s { DTP.tsCache = HM.insert key tix (DTP.tsCache s) }
+  return tix
 
 -- | Allocate a new dispatch table that can dispatch to the given payloads
 --
@@ -123,35 +124,74 @@ makePayload :: M.Map DTP.Pattern (DTP.LinkedTableIndex, e)
             -- ^ The bit we are matching against
             -> DTP.TrieM e (Bool, DTP.LinkedTableIndex)
 makePayload patterns bitIndex bitsSoFar bit =
-  case M.toList matchingPatterns of
-    [] -> return (bit, DTP.defaultElementIndex)
-    [(pat, (eltIdx, _elt))]
-      | patternBits pat - 1 == bitIndex -> return (bit, eltIdx)
-    _ | all ((> (bitIndex + 1)) . patternBits) (M.keys matchingPatterns) -> do
-          -- This case makes sure that we have covered all of the patterns
-          -- completely; we can't stop early, since there could be a required
-          -- fixed bit in the last position that might make a pattern fail even
-          -- if the entire prefix matches.
-          tix <- buildTableLevel matchingPatterns (bitIndex + 1) bitsSoFar'
-          return (bit, tix)
-      | M.null negativeMatchingPatterns -> return (bit, DTP.defaultElementIndex)
-      | Just (mostSpecificEltIdx, _) <- findMostSpecificPatternElt negativeMatchingPatterns -> do
-          return (bit, mostSpecificEltIdx)
-      | otherwise -> do
-          mapping <- St.gets DTP.tsPatternMnemonics
+  if | M.null matchingPatterns -> do
+         -- We failed to match any patterns (and eliminated all of the possible
+         -- patterns)
+         return (bit, DTP.defaultElementIndex)
+     | [(pat, (eltIdx, _elt))] <- M.toList minLengthPatterns
+     , patternBits pat - 1 == bitIndex -> do
+         -- In the above case we had a single match on the last bit.  This case
+         -- asks the question: if there is a collection of patterns where some
+         -- are shorter than the rest, do any of the shorter patterns match
+         -- conclusively at this bit pattern?  If so, that is also an answer.
+         return (bit, eltIdx)
+     | Just (mostSpecificEltIdx, _) <- findMostSpecificPatternElt minLengthPatterns -> do
+         -- It could be the case that multiple patterns happen to match.  In
+         -- those cases, we choose the unique pattern with the most required
+         -- bits.
+         return (bit, mostSpecificEltIdx)
+     | not (M.null longerPatterns) -> do
+         -- If we have no match and there are some patterns remaining that are
+         -- longer than the current bit string, extend the parse tree to cover
+         -- those patterns.
+         tix <- buildTableLevel matchingPatterns (bitIndex + 1) bitsSoFar'
+         return (bit, tix)
+     | otherwise -> do
+         mapping <- St.gets DTP.tsPatternMnemonics
 
-          let pats = map fst (M.toList negativeMatchingPatterns)
-          let mnemonics = catMaybes (flip M.lookup mapping <$> pats)
+         let pats = map fst (M.toList matchingPatterns)
+         let mnemonics = catMaybes (flip M.lookup mapping <$> pats)
 
-          error ("Overlapping bit pattern " ++ show (bitIndex, bitsSoFar, mapping, pats, mnemonics))
+         error ("Overlapping bit pattern " ++ show (bitIndex, bitsSoFar, mapping, pats, mnemonics))
   where
     bitsSoFar' = bitSnoc bitsSoFar bit
-    matchingPatterns' = M.filterWithKey (patternMatches bitIndex bit) patterns
-    matchLength = minimum (patternBits <$> M.keys matchingPatterns')
-    matchingPatterns = M.filterWithKey (\p _ -> patternBits p == matchLength) matchingPatterns'
-    -- Negative matching patterns are used to disambiguate when we are out of
-    -- bits
-    negativeMatchingPatterns = M.filterWithKey (negativePatternMatches bitsSoFar') matchingPatterns
+
+    -- All of the patterns that match the current bit prefix, purely considering
+    -- the positive patterns
+    --
+    -- Note that we don't pass in the entire bit prefix here: since we are in
+    -- this state, it means that the prefix matches all of the patterns we have
+    -- in @patterns@, so we don't need to re-examine those bits.  We only need
+    -- to look at the current bit.
+    matchingPositivePatterns = M.filterWithKey (patternMatches bitIndex bit) patterns
+    -- The remaining patterns that also satisfy any negative patterns that we
+    -- can apply at this point
+    --
+    -- We apply negative patterns as soon as possible: once we have enough bits
+    -- in the prefix to cover all of the required bits for the negative pattern
+    -- mask.
+    matchingNegativePatterns = M.filterWithKey (negativePatternMatches bitsSoFar') matchingPositivePatterns
+
+    -- Now find the length of the smallest matching pattern
+    --
+    -- If there are multiple matches and one is shorter than the rest, we can
+    -- use this to say that the unique shortest pattern supersedes the others.
+    -- minPatLen = minimum (patternBits <$> M.keys matchingNegativePatterns)
+
+    -- It could be the case that some patterns are shorter than others, and that
+    -- we hit a match to the shorter patterns early. We want to take those
+    -- matches first.
+    --
+    -- However, if the shorter set of patterns do not match, we want to continue
+    -- the search with the full set of possible patterns.  If we continue a
+    -- search past the size of a shortened pattern, we should remove the short
+    -- patterns entirely because they did not match (so they will not match)
+    minLengthPatterns = M.filterWithKey (\p _ -> patternBits p - 1 == bitIndex) matchingNegativePatterns
+
+    -- Remaining matching patterns longer than the current bit length
+    longerPatterns = M.filterWithKey (\p _ -> patternBits p >= bitIndex) matchingNegativePatterns
+
+    matchingPatterns = matchingNegativePatterns
 
 flattenTables :: DTP.LinkedTableIndex -> e -> DTP.TrieState e -> LT.LinearizedTrie e
 flattenTables t0 defElt finSt =
@@ -168,25 +208,46 @@ flattenTables t0 defElt finSt =
                   , e <- VU.toList tbl
                   ]
 
+-- | Return 'True' if the concrete bit value (at the specified index) satisfies
+-- the bit at that location in the 'DTP.Pattern'
+--
+-- If the pattern is not long enough to provide that bit, return False
 patternMatches :: Int -> Bool -> DTP.Pattern -> e -> Bool
-patternMatches bitIndex bit p _ =
-  (bit .&. patRequiredBit) == patTrueBit
+patternMatches bitIndex bit p _
+  | bitIndex >= patternBits p = False
+  | otherwise =
+    (bit .&. patRequiredBit) == patTrueBit
   where
     patRequiredBit = DTP.requiredMask p `bitIndexBytestring` bitIndex
     patTrueBit = DTP.trueMask p `bitIndexBytestring` bitIndex
 
+-- | Return True if the 'BitString' does not violate any negative patterns in the 'DTP.Pattern'
+--
+-- NOTE: Negative patterns can only reject a 'BitString' once all of the
+-- required bits in the negative bitmasks are present in the 'BitString'.  If
+-- the 'BitString' does not contain some of the required negative bit positions,
+-- this function simply returns True.
 negativePatternMatches :: BitString -> DTP.Pattern -> e -> Bool
 negativePatternMatches bs p _ =
   all (uncurry negativeMatch) (DTP.negativePairs p)
   where
     negativeMatch negMask negBits =
-      case all (==0) (BS.unpack negMask) of
-        True -> True
-        False ->
-          -- NOTE: the 'asBitList' function unrolls all of the bits in the
-          -- ByteString values; the zipWith just takes the necessary set given
-          -- the length of the bitstring.
-          not (and (zipWith3 negativeBitMatches (asBitList negMask) (asBitList negBits) (asBitList bs)))
+      let mHighestBit = highestRequiredBit negMask
+      in if | mHighestBit == Nothing ->
+              -- If there are actually no required bits in the negative mask, we
+              -- know it won't match anything
+              True
+            | Just highestBit <- mHighestBit
+            , bitStringLength bs < highestBit ->
+              -- If we don't have enough bits for the highest bit number in the
+              -- negative mask, it won't match yet
+              True
+            | otherwise ->
+              -- NOTE: the 'asBitList' function unrolls all of the bits in the
+              -- ByteString values; the zipWith just takes the necessary set
+              -- given the length of the bitstring.
+              not (and (zipWith3 negativeBitMatches (asBitList negMask) (asBitList negBits) (asBitList bs)))
+
     negativeBitMatches :: Bool -> Bool -> Bool -> Bool
     negativeBitMatches negBitMask negBitValue bit =
       (bit .&. negBitMask) == negBitValue
@@ -225,7 +286,16 @@ linkedToFlatIndex (DTP.LTI i)
 -- same here, but a more specific bitstring type is a bit more explicit and
 -- easier to follow
 newtype BitString = BitString (VU.Vector Bool)
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord)
+
+instance Show BitString where
+  show (BitString v) = "0b" ++ map asBitChar (VU.toList v)
+
+asBitChar :: Bool -> Char
+asBitChar b =
+  case b of
+    True -> '1'
+    False -> '0'
 
 bitSnoc :: BitString -> Bool -> BitString
 bitSnoc (BitString v) a = BitString (VU.snoc v a)
@@ -233,6 +303,19 @@ bitSnoc (BitString v) a = BitString (VU.snoc v a)
 bitEmpty :: BitString
 bitEmpty = BitString VU.empty
 
+bitStringLength :: BitString -> Int
+bitStringLength (BitString v) = VU.length v
+
+-- | Return the index of the highest 1 bit in the 'BS.ByteString'
+--
+-- If there are no 1 bits, returns Nothing
+highestRequiredBit :: BS.ByteString -> Maybe Int
+highestRequiredBit = fst . BS.foldl' highestSet (Nothing, 0)
+  where
+    highestSet (mIdx, byteNum) b =
+      let ix = 7 - countLeadingZeros b
+      in if | ix < 0 -> (mIdx, byteNum + 1)
+            | otherwise -> (Just (byteNum * 8 + ix), byteNum + 1)
 
 -- | Extract a bit (indexed from zero) from a bytestring
 --
@@ -247,7 +330,7 @@ bitEmpty = BitString VU.empty
 -- pattern.
 bitIndexBytestring :: BS.ByteString -> Int -> Bool
 bitIndexBytestring bs i =
-  X.assert (i >= 0) $ testBit byte bitIndex
+  X.assert (i >= 0) $ X.assert (BS.length bs > byteIndex) $ testBit byte bitIndex
   where
     (byteIndex, bitIndex) = i `divMod` 8
     byte = bs `BS.index` byteIndex
@@ -269,3 +352,27 @@ flipBits w = [ testBit w i | i <- [0..7] ]
 
 instance AsBitList BitString where
   asBitList (BitString v) = VU.toList v
+
+{- [BitTrie Design]
+
+This implementation is inspired by the ByteTrie, but instead of dispatching
+based on byte values at each level of the tree, it dispatches one bit at a time.
+
+Some important semantic notes for the table construction process:
+
+- Negative patterns can only be matched once all of their required bits are
+  present.  It would probably be best to match them as soon as possible to
+  simplify the logic and reduce the size of matched pattern sets.
+
+- If table results are cached, the cache must respect negative patterns.  If we
+  don't respect negative patterns, there will be invalid patterns in the suffix
+  returned by the cache lookup
+
+  Instead of invalidating the cache at each lookup, it might be more efficient
+  to compute a predicate for cached entries that returns 'True' if the entry is
+  not valid for the "current"
+
+- We can match a negative pattern once we have reached the last set bit in the
+  negative pattern mask
+
+-}
